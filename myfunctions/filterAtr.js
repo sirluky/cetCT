952533d9bcee16f844683dee1254b9039e2c62ca
origin <lkov@post.cz>
2019-02-07 23:01:06 +0100

    filterFunction deleteArray support

--- myfunctions/filterAtr.js
+++ myfunctions/filterAtr.js
@@ -3,22 +3,28 @@
  * @param {*} content html obsah, ktery jsme dostali z cms
  * @param {*} toDelete atributy, ktere chceme smazat
  */
-function filterAtr(content, toDelete = "srcset") {
-  let out = "";
-  let toFind = toDelete + '="';
-  let from = content.search(toFind);
+function filterAtr(content, toDeletearr = ["srcset"]) {
+  toDeletearr.forEach(toDelete => {
+    let out = "";
+    let toFind = toDelete + '="';
+    let from = content.search(toFind);
 
-  while (from >= 0) {
-    let to =
-      content.slice(from + toFind.length + 1, content.length - 1).search('"') +
-      from +
-      toFind.length +
-      2;
-    out += content.slice(0, from);
-    content = content.slice(to, content.length);
-    from = content.search(toFind);
-  }
-  out += content;
+    while (from >= 0) {
+      let to =
+        content
+          .slice(from + toFind.length + 1, content.length - 1)
+          .search('"') +
+        from +
+        toFind.length +
+        2;
+      out += content.slice(0, from);
+      content = content.slice(to, content.length);
+      from = content.search(toFind);
+    }
+    out += content;
+    content = out;
+  });
+
   return out;
 }
 module.exports = filterAtr;
