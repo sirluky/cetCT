a7224f4efb26b4782bde6a510185c18ee1d9eac3
origin <lkov@post.cz>
2019-02-09 23:21:53 +0100

    fixed page number (now goes from 1)

--- nodes/buildpaging.js
+++ nodes/buildpaging.js
@@ -26,7 +26,8 @@ const buildPaging = (createPage, posts, blogPost, blogpaged) => {
   stranky.push(stranka);
   stranka = [];
   stranky.forEach((str, index) => {
-    let cislo = index === 0 ? "" : index;
+    let cislo = index === 0 ? "" : "page/" + index;
+
     createPage({
       path: `/${cislo}`,
       component: blogpaged,
