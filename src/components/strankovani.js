a479090ac35694875f972c7087db31ba0920f505
origin <lkov@post.cz>
2019-02-09 23:22:10 +0100

    fixed paging number

--- src/components/strankovani.js
+++ src/components/strankovani.js
@@ -5,14 +5,14 @@ export default ({ n, maxn }) => (
   <div className="strankovani">
     {" "}
     {n - 1 >= 0 ? (
-      <Link className="pred" to={`/${n - 1 > 0 ? n - 1 : "/"}`}>
+      <Link className="pred" to={`/${n - 1 > 0 ? "page/" + n : ""}`}>
         Předchozí stránka
       </Link>
     ) : (
       <p className="pred"> Minulá stránka</p>
     )}
     {n + 1 < maxn ? (
-      <Link className="dalsi" to={`/${n + 1}`}>
+      <Link className="dalsi" to={`/page/${n + 2}`}>
         Další stránka
       </Link>
     ) : (
